751876f545af9e353d728cd31698c482ce99a50a
David Quiñones <daquinons@users.noreply.github.com>
2020-04-02 13:40:16 -0500

    Remove sandbox to build flatpak

--- main.js
+++ main.js
@@ -254,6 +254,8 @@ function createWindow() {
   app.tray.setContextMenu(contextMenu);
 }
 
+app.commandLine.appendSwitch('--no-sandbox');
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
